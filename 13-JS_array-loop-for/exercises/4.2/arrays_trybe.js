cd6c2772a6f2c152bab467815f0e1c64607f976d
Jesus Palli <jepaur5@gmail.com>
2020-07-14 18:36:47 -0300

    terminado aula 13

---
+++ 13-JS_array-loop-for/exercises/4.2/arrays_trybe.js
@@ -0,0 +1,122 @@
+
+//question1
+//let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27 ];
+
+//for ( i= 0; i <= 9; i++) {
+//    console.log(numbers[i]);
+//}
+
+
+
+
+//question 2
+//let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27 ];
+//let result = 0;
+//for ( ii= 0; ii <= 9; ii++) {
+    
+// result = result + numbers[ii];
+    /////console.log(result);
+//}
+//console.log(result);
+
+
+
+
+//question3 media aritmetica e é maior o menor que 20
+//let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27 ];
+//let suma = 0
+//for ( i = 0; i <= 9; i++){
+//    suma = suma + numbers[i];
+//    media = suma / numbers.length;
+//}
+//console.log(media);
+
+//if (media > 20 ){
+//    console.log('valor maior que 20');
+//}
+//else {
+//    console.log('valor menor o igual que 20')
+//}
+
+
+
+
+//question 5 maior valor contido no array
+// **explicacao: 
+// ** SI : number[0] > higherNumber 
+//             5  > 0  ===>> O novo higherNUmber é 5
+// ** SI:  number[1] > higherNumber
+//             9  > 5  ===>> O novo higherNUmber é 9
+// ...
+// ...
+// ** SI:  number[6] > higherNumber
+//             8  > 100  ===>> NÃO É CERTO. 
+//                             Então: console.log(higherNumber)
+//                                    console.log(100)
+
+//resposta questao 5
+//let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27 ];
+//let higherNumber = 0;
+
+//for (let i = 0; i < numbers.length; i++){
+//    if (numbers[i] > higherNumber){
+//        higherNumber = numbers[i];    }
+//}
+
+//console.log(higherNumber);
+
+
+
+
+
+//question 6 - valores impares
+//let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27 ];
+//let result = 0;
+
+//for(let i= 0; i < numbers.length; i++){
+//    if (numbers[i] % 2 !=0){
+//        result = result + 1;
+//    }
+//}
+
+//if(result === 0){
+//    console.log('nemhum valor impar encontrado');
+//}
+//else{
+//    console.log(result);
+//}
+
+
+
+
+//question 7 - menor valor 
+// o valor da variavel smallNumber tem que ser um numero
+// maior que qualquer numero que faz parte do array
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27 ];
+//let smallNumber = 1000
+//for ( i = 0; i <= numbers.length ;)
+
+console.log(numbers.length)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
